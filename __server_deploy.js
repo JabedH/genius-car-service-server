7b29d7ca9f9f4fd1a60cdd364e61ac62cecb0246
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-26 22:31:22 +0600

    'added'

---
+++ __server_deploy.js
@@ -0,0 +1,11 @@
+/** ----------
+ * ONE TIME
+ * -----------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku
+ * 4. heroku login
+ * ---------
+ * for each project
+ * -------------
+ */
